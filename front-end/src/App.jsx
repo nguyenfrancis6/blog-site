d3787f8af31b714a2348f58361bc57645045ef09
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-02-12 13:52:39 -0800

    Configured routes, created layout component to add navbar to all routes using Outlet and adding children to routes

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,33 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Layout from './Layout';
+import AboutPage from './pages/AboutPage';
+import ArticlePage from './pages/ArticlePage';
+import ArticlesList from './pages/ArticlesList';
+import HomePage from './pages/HomePage'
+import {
+  createBrowserRouter, 
+  RouterProvider,
+} from 'react-router-dom'
+
+const routes = 
+  [{
+    path: '/', 
+    element: <Layout />,
+    children: [{
+      path: '/',
+      element: <HomePage />
+    }, 
+    {
+      path: '/about',
+      element: <AboutPage />
+    }, 
+    {
+      path: '/articles',
+      element: <ArticlesList />
+    }, 
+    {
+      path: '/articles/individual',
+      element: <ArticlePage />
+    }]
+  }]
+  
+
+const router = createBrowserRouter(routes);
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR, yo!!!
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={router} />
     </>
   )
 }
