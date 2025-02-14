c76326908da3a3ded08275347d529941d4d3a2fa
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-02-14 00:27:39 -0800

    temporary data, created endpoint to upvote articles using route parameters

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,19 +1,20 @@
 import express from 'express';
 
+const articleInfo = [
+  { name: 'learn-node', upvotes: 0 },
+  { name: 'learn-react', upvotes: 0 },
+  { name: 'mongodb', upvotes: 0 },
+]
+
 const app = express();
 
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  res.send('Hello, ' + req.body.name + ' from a GET endpoint!');
-})
+app.post('/api/articles/:name/upvote', (req, res) => {
+  const article = articleInfo.find(a => a.name === req.params.name)
+  article.upvotes += 1;
 
-app.get('/hello/:name', (req, res) => {
-  res.send("Hello, " + req.params.name)
-})
-
-app.post('/hello', (req, res) => {
-  res.send("Hello from a POST endpoint!")
+  res.send('Success! The article ' + req.params.name + " now has " + article.upvotes + ' upvotes!')
 })
 
 app.listen(8000, () => {
