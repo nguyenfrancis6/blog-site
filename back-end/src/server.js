1c4bd87329e921131fa6a6ff7b2482abb88947b1
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-02-14 03:06:37 -0800

    Connect mongodb to express, add sample data into collection

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,4 +1,5 @@
 import express from 'express';
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
 const articleInfo = [
   { name: 'learn-node', upvotes: 0, comments: [] },
@@ -10,16 +11,38 @@ const app = express();
 
 app.use(express.json());
 
+app.get('/api/articles/:name', async (req, res) => {
+  const { name } = req.params;
+
+  const uri = 'mongodb://127.0.0.1:27017';
+
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  })
+
+  await client.connect();
+
+  const db = client.db('react-full-stack-db')
+
+  const article = await db.collection('articles').findOne({ name })
+
+  res.json(article);
+})
+
 app.post('/api/articles/:name/upvote', (req, res) => {
   const article = articleInfo.find(a => a.name === req.params.name)
   article.upvotes += 1;
   
-  res.json(article);
+  res.json(article); // sends back json info
 })
 
 app.post('/api/articles/:name/comments', (req, res) => {
-  const name = req.params.name
-  const { postedBy, text } = req.body;
+  const name = req.params.name // get name from route
+  const { postedBy, text } = req.body; // tested with postman
 
   const article = articleInfo.find(a => a.name === name)
 
@@ -28,7 +51,7 @@ app.post('/api/articles/:name/comments', (req, res) => {
     text,
   })
 
-  res.json(article)
+  res.json(article) // sends back json info
 })
 
 app.listen(8000, () => {
