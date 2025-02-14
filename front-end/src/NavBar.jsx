936f1ced60627425c43862a937ecd10307fffb0e
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-02-14 09:24:48 -0800

    changed navbar interface to sign in/sign out + custom useUser hook to get state of user and loading

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -1,15 +1,43 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { getAuth, signOut } from "firebase/auth";
+import useUser from "./useUser";
 
 const NavBar = () => {
+  const { isLoading, user } = useUser();
+
+  const navigate = useNavigate();
+
   return (
     <nav>
       <ul>
-        <li><Link to='/'>Home</Link></li>
-        <li><Link to='/about'>About</Link></li>
-        <li><Link to='/articles'>Articles</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/articles">Articles</Link>
+        </li>
+        {isLoading ? (
+          <li>Loading</li>
+        ) : (
+          <>
+            {user && (
+              <li style={{ color: "white" }}>Logged in as {user.email}</li>
+            )}
+            <li>
+              {user ? (
+                <button onClick={() => signOut(getAuth())}>Sign Out</button>
+              ) : (
+                <button onClick={() => navigate("/login")}>Sign In</button>
+              )}
+            </li>
+          </>
+        )}
       </ul>
     </nav>
-  )
-}
+  );
+};
 
-export default NavBar
+export default NavBar;
