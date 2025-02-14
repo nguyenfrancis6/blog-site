b73531654f6de4c5c20758340eafdcdf00f229b1
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-02-13 20:37:08 -0800

    Testing with postman

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -2,8 +2,14 @@ import express from 'express';
 
 const app = express();
 
+app.use(express.json());
+
 app.get('/hello', (req, res) => {
-  res.send('Hello!');
+  res.send('Hello, ' + req.body.name + ' from a GET endpoint!');
+})
+
+app.post('/hello', (req, res) => {
+  res.send("Hello from a POST endpoint!")
 })
 
 app.listen(8000, function() {
