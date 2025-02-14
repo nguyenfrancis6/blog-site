a89c0f68b14728611f5e58a56a7b1bc8a744ea0c
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-02-14 00:21:22 -0800

    Route parameters in express

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -8,10 +8,14 @@ app.get('/hello', (req, res) => {
   res.send('Hello, ' + req.body.name + ' from a GET endpoint!');
 })
 
+app.get('/hello/:name', (req, res) => {
+  res.send("Hello, " + req.params.name)
+})
+
 app.post('/hello', (req, res) => {
   res.send("Hello from a POST endpoint!")
 })
 
-app.listen(8000, function() {
+app.listen(8000, () => {
   console.log("server is listening on port 8000");
 })
