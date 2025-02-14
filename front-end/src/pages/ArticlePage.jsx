a8df41e2af3b21ad7813e3ea8704483213f3b58b
Francis Nguyen <nguyenfrancis6@gmail.com>
2025-02-14 03:40:54 -0800

    Create commentslist + upvote button and functionality with useState and axios

--- front-end/src/pages/ArticlePage.jsx
+++ front-end/src/pages/ArticlePage.jsx
@@ -1,19 +1,30 @@
+import { useState } from "react";
 import { useParams, useLoaderData } from "react-router-dom";
 import articles from "../article-content";
+import CommentsList from "../CommentsList";
 import axios from "axios";
 
 const ArticlePage = () => {
   const { name } = useParams();
-  const { upvotes, comments } = useLoaderData();
+  const { upvotes: initialUpvotes, comments } = useLoaderData();
+  const [upvotes, setUpvotes] = useState(initialUpvotes);
   const article = articles.find((a) => a.name === name);
+
+  async function onUpvoteClicked() {
+    const response = await axios.post('/api/articles/' + name + '/upvote');
+    const updatedArticleData = response.data;
+    setUpvotes(updatedArticleData.upvotes);
+  }
 
   return (
     <>
       <h1>{article.title}</h1>
+      <button onClick={onUpvoteClicked}>Upvote</button>
       <p>This article has {upvotes} upvotes!</p>
       {article.content.map((p) => (
         <p key={p}>{p}</p>
       ))}
+      <CommentsList comments={comments} />
     </>
   );
 };
